2065d6598dc82d42d30f4f7f292012ad0f972209
Cleverson Lira <cleverson_lira@outlook.com>
2018-02-01 22:05:46 -0200

    Adicionando efeito visual na remoção do paciente com classList.add() e setTimeout(action, time)

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,9 +1,13 @@
 var pacientes = document.querySelectorAll(".paciente");
 
 var table = document.querySelector("#tabela-pacientes");
+//var table = document.querySelector("table"); Neste caso um if(event.target.tagName == 'TD') seria necessário para não apagar também o cabeçalho
 
 table.addEventListener("dblclick", function(event) {
-	event.target.parentNode.remove(); //Remova o pai do alvo do evento (Right to Left)! 
+	event.target.parentNode.classList.add("fadeOut"); // Adiciona a classe .fadeOut no pai do alvo do evento;
+	setTimeout(function() { //Aguarda 500 milisegundos para executar o que foi descrito na functio anonima passada como primeiro parâmetro
+		event.target.parentNode.remove(); //Remova o pai(tr) do alvo(td) do evento(click) - (Right to Left)!
+	}, 500);
 });
 
 /*function addRemotion(pacientes) {
